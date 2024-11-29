e51bc13c72e1f479d22c071d663b56da2192962e
TrappedWhale <trapinwhale@gmail.com>
2024-11-29 23:03:21 +0900

    first open

---
+++ scripts/refer.gd
@@ -0,0 +1,16 @@
+extends Node
+"""
+cd documents/godot/roguelike
+
+git init
+git add .
+git status
+git commit -m "first open"
+
+git remote add origin https://github.com/trappedwhale/RogueLike.git
+git branch -M main
+git push -u origin main
+git pull origin main --rebase
+git push -u origin main
+
+"""
